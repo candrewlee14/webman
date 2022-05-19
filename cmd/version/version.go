c3060192f67fc61ebe03aab79b364fb1526de20c
Andrew Lee <candrewlee14@gmail.com>
2022-05-19 12:25:49 -0600

    fix: solve version slice problem

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -25,9 +25,17 @@ The "version" subcommand displays the latest webman version.`,
 			cmd.Help()
 			os.Exit(0)
 		}
-		color.Cyan("webman (v%s)", Version)
-		color.Yellow("Commit %s", Commit[:8])
-		color.Magenta("Built on %s by %s", Date[:10], BuiltBy)
+		color.Cyan("webman (%s)", Version)
+		verLen := 8
+		if len(Commit) < 8 {
+			verLen = len(Commit)
+		}
+		color.Yellow("Commit %s", Commit[:verLen])
+		dateLen := 10
+		if len(Date) < 10 {
+			dateLen = len(Date)
+		}
+		color.Magenta("Built on %s by %s", Date[:dateLen], BuiltBy)
 		color.HiBlack("Created by candrewlee14")
 	},
 }
