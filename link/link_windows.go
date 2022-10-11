8542c17c1df133ff130931b5ffce4f6aa6f69eb5
enr <enrico.comiti@gmail.com>
2022-10-11 14:05:32 +0200

    windows cmd if symlink fails

--- link/link_windows.go
+++ link/link_windows.go
@@ -6,25 +6,32 @@ package link
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
-// If symlink creation fails, fallbacks to `.bat` script.
+// If symlink creation fails, fallbacks to creating a script named `${executable name minus extension}.cmd`.
 func symlink(old string, new string) (bool, error) {
 	err := os.Symlink(old, new)
 	if err == nil {
 		return true, nil
 	}
 
-	f, err := os.Create(new + ".bat")
+	f, err := os.Create(cmdPath(new))
 	if err != nil {
 		return false, err
 	}
 	defer f.Close()
 	_, err = f.WriteString(
-		fmt.Sprintf("@echo off\n%s", old) + ` %*`,
+		fmt.Sprintf("@echo off\r\n%s %s\r\n", old, `%*`),
 	)
 	if err != nil {
 		return false, err
 	}
 	return true, nil
+}
+
+func cmdPath(orig string) string {
+	fmt.Printf("   orig %s \n", orig)
+	noext := orig[:len(orig)-len(filepath.Ext(orig))]
+	return fmt.Sprintf(`%s.cmd`, noext)
 }
